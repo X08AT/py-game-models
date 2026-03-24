9081f1a126f23146ab6b8320a209b1017e1ad500
Artem <Artem.at131@gmail.com>
2026-03-24 21:15:59 +0200

    update 2

--- main.py
+++ main.py
@@ -9,30 +9,36 @@ def main() -> None:
         players_data = json.load(file)
 
     for nickname, data in players_data.items():
+        race_data = data.get("race")
+        if not race_data:
+            continue
+
         race, _ = Race.objects.get_or_create(
-            name=data["race"]["name"],
-            defaults={"description": data["race"]["description"]}
+            name=race_data.get("name"),
+            defaults={"description": race_data.get("description", "")}
         )
 
-        for skill_data in data["race"]["skills"]:
+        skills_list = race_data.get("skills", [])
+        for skill_info in skills_list:
             Skill.objects.get_or_create(
-                name=skill_data["name"],
+                name=skill_info.get("name"),
                 race=race,
-                defaults={"bonus": skill_data["bonus"]}
+                defaults={"bonus": skill_info.get("bonus")}
             )
 
+        guild_data = data.get("guild")
         guild = None
-        if data["guild"]:
+        if guild_data:
             guild, _ = Guild.objects.get_or_create(
-                name=data["guild"]["name"],
-                defaults={"description": data["guild"]["description"]}
+                name=guild_data.get("name"),
+                defaults={"description": guild_data.get("description")}
             )
 
         Player.objects.get_or_create(
             nickname=nickname,
             defaults={
-                "email": data["email"],
-                "bio": data["bio"],
+                "email": data.get("email"),
+                "bio": data.get("bio", ""),
                 "race": race,
                 "guild": guild
             }
